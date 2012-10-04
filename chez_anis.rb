3fb294266a6ce9fb93af5e612a1898128f0d5465
Anis Zarei <azarei@gettyimages.com>
2012-10-04 16:34:05 -0600

    Committing some training stuff for ruby

---
+++ chez_anis.rb
@@ -0,0 +1,105 @@
+# class Bakery
+#     def bread_delivery(bread_type)
+#         bread_inventory = {"white" => 12, "brown" => 12, "rye" => 12, "sourdough" => 12}
+#         bread_available = bread_inventory.has_key?(bread_type)
+#         return self
+#     end
+# end
+
+
+bread_inventory = Pantry.new(:bread => {"white" => 12, "brown" => 12, "rye" => 12, "sourdough" => 12}, :meat => {"salami" => 10, "turkey" => 10, "ham" => 10}, :veggie => {"tomato" => 5, "lettuce" => 5, "pickle" => 5})
+
+
+class Ingredient
+    attr_reader :name, :type
+    def initialize(name, type)
+        @name = name
+        @type = type
+    end 
+end
+
+class Pantry
+    def initialize
+        @@ingredients = Hash.new
+    end
+    def add(ingredient)
+        ## add the unique ingredient by name to the pantry
+        @@ingredients[ingredient.name] = ingredient
+    end
+    def get(name)
+        @@ingredients[name]
+        #returns object or nil
+    end
+    def contains
+        @@ingredients.keys
+    end
+    def types()
+        types = @@ingredients.each_value { |ingredient| ingredient.type}
+        type.uniq
+        # return what types of ingredients we have
+    end 
+
+    def getBread
+        # do we even know about bread
+        return nil unless self.types.contains?('bread')
+
+    end 
+end
+
+
+class Sandwich
+    def add(ingredient)
+
+    end
+end
+
+# class ChezAnis
+#     def check_inventory(bread_type, meat_type, veggie_type)
+#         complete_inventory = [bread_inventory, meat_inventory, veggie_inventory]
+
+#         bread_available = bread_inventory.has_key?(bread_type)
+#         meat_available = meat_inventory.has_key?(meat_type)
+#     end
+
+#     def bread_choice(bread_type)
+#         # Bakery.bread_delivery('bread_type')
+
+#         if bread_available == true
+#             puts "Great choice! #{bread_type} it is!"
+#             bread_go = true
+#         else
+#             puts "Sorry, I don't have that kind of bread. Your choices are " + bread_inventory.keys.join(", ") + "."
+#             bread_go = false
+#         end
+#         return self
+#     end
+
+#     def meat_choice(meat_type,slices)
+#         case slices
+#         when 1
+#             puts "then add one slice of #{meat_type}"
+#         when nil
+#             puts "doesn't look like you wanted any meat"
+#         else
+#             puts "then add #{slices} slices of #{meat_type}"
+#         end
+
+#         return self
+#     end
+#     def veggie_choice(veggie_type, veggie_amount)
+#         if veggie_amount != nil
+#             puts "toss in #{veggie_amount} of #{veggie_type}"
+#         else
+#             puts "toss in some #{veggie_type}"
+#         end
+#         return self
+#     end
+#     def condiment_choice(condiment)
+#         puts "then we'll top it of with some #{condiment}"
+#         return self
+#     end
+
+
+# end #class ChezAnis
+
+# anis = ChezAnis.new
